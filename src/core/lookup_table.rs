e090d36856d4ae37f4722844b2bd2c4dd2f69f39
Jérôme Ramette <jerome.ramette@gmail.com>
2023-12-13 22:32:44 +0100

    first impl

--- src/core/lookup_table.rs
+++ src/core/lookup_table.rs
@@ -7,19 +7,27 @@ struct LookupTable3d<T: Clone> {
 }
 
 impl<T: Clone> LookupTable3d<T> {
-    fn new(table: Vec<T>, right: usize, center: usize, left: usize) -> Self {
+    fn new(right: usize, center: usize, left: usize, default_value: T) -> Self {
         Self {
-            table,
+            table: vec![default_value; right * center * left],
             right,
             center,
             left,
         }
     }
 
+    fn set(&mut self, right: usize, center: usize, left: usize, value: T) {
+        let index = self.index(right, center, left);
+        self.table[index] = value;
+    }
+
     fn get(&self, right: usize, center: usize, left: usize) -> T {
         let index = self.index(right, center, left);
         self.table[index].clone()
     }
+
+    //TODO: make this private
+    //TODO: fix index calculation
 
     fn index(&self, right: usize, center: usize, left: usize) -> usize {
         let index = right * self.right + center * self.center + left * self.left;
